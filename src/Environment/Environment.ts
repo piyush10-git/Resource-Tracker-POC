e9fcafeb45ef8c2e788a90cd00247d1caf54de79
Piyus Pise <ppise@ctepl.com>
2025-08-10 22:46:05 +0530

    chore: updated url

--- src/Environment/Environment.ts
+++ src/Environment/Environment.ts
@@ -1,4 +1,4 @@
 export const Environment = {
-    URI: "http://localhost:5150",
-    // URI: "https://resourcetrackerbackendpiyu-dmgefaczfmeyd3hs.canadacentral-01.azurewebsites.net", // backend uri
+    // URI: "http://localhost:5150",
+    URI: "https://resourcetrackerbackendpiyu-dmgefaczfmeyd3hs.canadacentral-01.azurewebsites.net", // backend uri
 }
